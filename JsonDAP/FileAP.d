7617fba0ff9a0932d983ca81516ea471d50aa454
khashayar <aripastudio44@gmail.com>
2025-07-12 19:24:05 -0700

    Fix Main Dll

--- JsonDAP/FileAP.d
+++ JsonDAP/FileAP.d
@@ -1,1 +1,85 @@
 module FileAP;
+
+
+import std.file;
+import std.json;
+import std.typecons;
+import std.exception;
+import PublicCodeOtherCode;
+
+alias Optional = Nullable;
+
+public static class CL_FileAP
+{
+	public static Optional!T readJsonFile(string filePath)(){
+		try{
+			enforce(exists(filePath)) , new FileException("File not found.",filePath);
+			string jsonContent = readText(filePath);
+			return deserializeJson!T(jsonContent);
+		}catch(FileException  e)
+		{
+			throw new JsonOperationException("FileAP Error: File I/O error reading : "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}catch(JSONException  e)
+		{
+			throw new JsonOperationException("FileAP Error: Invalid JSON format in :"~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}catch(Exception e)
+		{
+			throw new JsonOperationException("FileAP Error: Unexpected error reading : "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}
+	}
+
+	public static bool writeJsonFile(T)(string filePath , T data)
+	{
+		try{
+			enforce(exists(filePath)) , new FileException("File not found.",filePath);
+			string jsonContent = serializeToJson(data);
+			writeText(filePath , jsonContent);
+		}catch(FileException  e)
+		{
+			throw new JsonOperationException("FileAP Error: File I/O error writing to : "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}catch(JSONException  e)
+		{
+			throw new JsonOperationException("FileAP Error: Serialization error writing to :"~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}catch(Exception e)
+		{
+			throw new JsonOperationException("FileAP Error: Unexpected error writing to  : "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}
+	}
+
+
+	public static Optional!long getJsonFileSize(string filePath)
+	{
+		try
+		{
+			enforce(exists(filePath), new FileException("File not found for size.", filePath));
+			long fileSize = std.file.getSize(filePath);
+			return Optional!long(fileSize);
+		}catch(FileException  e)
+		{
+			throw new JsonOperationException("FileAP Error: File I/O error getting size of: "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}catch(Exception e)
+		{
+			throw new JsonOperationException("FileAP Error: Unexpected error getting size of   : "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}
+	}
+
+	public static Optional!(T[]) readJsonArray(string filepath)()
+	{
+		try{
+
+		}catch(FileException  e)
+		{
+			throw new JsonOperationException("FileAP Error: File I/O error reading array from: "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}catch(JSONException  e)
+		{
+			throw new JsonOperationException("FileAP Error: Invalid JSON format for array in :"~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}catch(Exception e)
+		{
+			throw new JsonOperationException("FileAP Error: Unexpected error reading array from  : "~ filePath ~ "Error : " ~ e.msg, __FILE__, __LINE__, e);			
+		}
+	}
+
+
+
+
+}
--- JsonDAP/JsonDAP.d
+++ JsonDAP/JsonDAP.d
@@ -1,9 +1,69 @@
 module JsonDAP;
 
 import std.stdio;
+import CoreAP;
+import FileAP;
+import PublicCodeOtherCode;
 
-export int foo()
+import std.typecons; 
+
+alias Optional = Nullable;
+
+public class JsonAP
 {
-	return 42;
-}
+	export static Optional!T APreadJsonFile(string filePath)() {
+        return CL_FileAP.readJsonFile!T(filePath);
+    }
 
+    export static bool APwriteJsonFile(T)(string filePath, T data) {
+        return CL_FileAP.writeJsonFile!T(filePath, data);
+    }
+
+    export static Optional!long APgetJsonFileSize(string filePath) {
+        return CL_FileAP.getJsonFileSize(filePath);
+    }
+
+    export static Optional!(T[]) APreadJsonArray(string filePath)() {
+        return CL_FileAP.readJsonArray!T(filePath);
+    }
+
+    export static bool APisValidJson(string jsonContent) {
+        return CL_CoreAP.isValidJson(jsonContent);
+    }
+
+    export static Optional!long APgetJsonItemCount(string jsonContent) {
+        return CL_CoreAP.getJsonItemCount(jsonContent);
+    }
+
+    export static bool APisJsonNumber(string jsonContent) {
+        return CL_CoreAP.isJsonNumber(jsonContent);
+    }
+
+    export static bool APisJsonString(string jsonContent) {
+        return CL_CoreAP.isJsonString(jsonContent);
+    }
+
+    export static Optional!string APprettyPrintJson(string jsonContent) {
+        return CL_CoreAP.prettyPrintJson(jsonContent);
+    }
+
+    export static Optional!V APgetJsonValue(string jsonContent, string path)() {
+        return CL_CoreAP.getJsonValue!V(jsonContent, path);
+    }
+
+    export static bool APisJsonObject(string jsonContent) {
+        return CL_CoreAP.isJsonObject(jsonContent);
+    }
+
+    export static bool APisJsonArray(string jsonContent) {
+        return CL_CoreAP.isJsonArray(jsonContent);
+    }
+
+    export static bool APisDigitAP(dchar c){
+        return CL_PublicCodeOtherCode.isDigitAP(c);
+    }
+
+    export static bool APisInteger(string s){
+        return CL_PublicCodeOtherCode.isInteger(s);
+    }
+}
