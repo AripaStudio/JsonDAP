51d6a8d8aa2aec2bff3feeac1988c16bd6b102ce
khashayar <aripastudio44@gmail.com>
2025-07-22 07:37:50 -0700

    add New Exception : ConvertExceptionAP

--- JsonDAP/ExceptionAP.d
+++ JsonDAP/ExceptionAP.d
@@ -65,3 +65,49 @@ public class UnknownErrorexceptionAP : Exception{
 		super(msg , file , line , next);
 	}
 }
+
+
+public class ConvertExceptionAP : Exception{
+    string parameterName;
+    string invalidValue;
+    string InputType;
+    string OutPutType;
+    this(string msg , string parameterName , string InputType , string OutputType , string invalidValue , 
+		 string file = __FILE__ , size_t line = __LINE__ , Throwable next = null)pure{
+            super(msg,file,line,next);
+            this.parameterName = parameterName;
+            this.invalidValue = invalidValue;
+            this.InputType = InputType;
+            this.OutPutType = OutputType;
+		 }
+    this(string msg,string parameterName , string invalidValue , string InputType , string OutputType )pure
+	{
+        this(msg,parameterName,invalidValue , InputType , OutputType , __FILE__ , __LINE__ , null);
+	}
+
+    override string toString(){
+        return super.toString() ~ 
+            "\n Parameter Name : " ~ parameterName ~
+            "\n Invalid  Value : " ~ invalidValue ~ 
+			"\n Input Type : "  ~ InputType ~ 
+            "\n Error Output Type : " ~ OutPutType;
+            
+	}
+
+    string getParameterName() const {
+        return parameterName;
+    }
+
+    string getInvalidValue() const {
+        return invalidValue;
+    }
+    
+    string getInputType() const {
+        return InputType;
+	}
+    
+    string getOutputType() const {
+        return OutPutType;
+	}
+
+}
